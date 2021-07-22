c90f91e77e7f418ea7d66c3beabbe8b21f0ae94d
gastonsk <amayagaston.sk@gmail.com>
2021-07-21 23:19:45 -0300

    name changes on some files
    - images located in the wrong place with the wrong name
    
    + image located in the right place with the right name

--- src/components/person/CardPerson.jsx
+++ src/components/person/CardPerson.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../styles/cardPerson.css';
 
-export default function CardPerson ({ persona }) {
+export default function CardPerson ({ persona, libro }) {
     return (
         <div className="container">
-            <div key={persona.id} className="card">
-                <div className="cardBackGround">
-                    <h2 className="tittleBook">Nombre: {persona.nombre}</h2>
-                    <h2 className="surnamePerosn">Apellido: {persona.apellido}</h2>
+            <div key={persona.id} className="cardPerson">
+                <div>
+                    <h2 className="namePerson">Nombre: {persona.nombre}</h2>
+                    <h2 className="surnamePerson">Apellido: {persona.apellido}</h2>
+                    <h2 className="emailPerson">Email: {persona.email}</h2>
+                    <h2 className="aliasPerson">Alias: {persona.alias}</h2>
+                    <h2 className="personaID">ID: {persona.ID}</h2 >
+                    
+
                     
                     
                     
