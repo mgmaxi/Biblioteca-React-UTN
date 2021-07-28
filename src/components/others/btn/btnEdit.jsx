466429c60e65aa0185e2c795af9ea35a2ea82751
mgmaxi <maximiliano.ghirardi@gmail.com>
2021-07-28 18:24:17 -0300

    btn props onClick

--- src/components/others/btn/btnEdit.jsx
+++ src/components/others/btn/btnEdit.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 /* Styles */
 import './styles/btn.css';
 
-export default function Edit (){
+export default function Edit (props){
     return (
         <div className="btnContainer">
-            <i className="fa fa-pencil btn" aria-hidden="true"></i>
+            <i onClick={props.onClick} className="fa fa-pencil btn" aria-hidden="true"></i>
             <p>Modificar</p>
         </div>
     )
--- src/components/others/btn/btnReturn.jsx
+++ src/components/others/btn/btnReturn.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 /* Styles */
 import './styles/btn.css';
 
-export default function Return (){
+export default function Return (props){
     return (
         <div className="btnContainer">
-            <i className="fa fa-share btn" aria-hidden="true"></i>
+            <i onClick={props.onClick} className="fa fa-share btn" aria-hidden="true"></i>
             <p>Devolver</p>
         </div>
     )
